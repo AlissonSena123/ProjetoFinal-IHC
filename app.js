714944194921f9222705040b0a1e9bc5a2ab7e57
Alisson <alissonsena784@gmail.com>
2026-05-16 17:42:34 -0300

    tela de cadastro

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const pages = require("./src/routes/pages.js");
 const PORT = 8080;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/", pages);
 
-app.listen(PORT, () => console.log(`SERVIDOR RODANDO COM SUCESSO. http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`SERVIDOR RODANDO COM SUCESSO. http://localhost:${PORT}/cadastrar`));
 
 
---
+++ src/routes/pages.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+
+router.get("/cadastrar", (req, res) => res.sendFile(path.join(__dirname, "../../views/cadastro.html")));
+router.get("/login", (req, res) => res.sendFile(path.join(__dirname, "../../views/login.html")));
+
+module.exports = router;
