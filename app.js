bbef7365455849fac6a748d14ffb07ed6ea40417
Alisson <alissonsena784@gmail.com>
2026-05-17 10:02:30 -0300

    .

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 8080;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/", pages);
+app.use(pages);
 
 app.listen(PORT, () => console.log(`SERVIDOR RODANDO COM SUCESSO. http://localhost:${PORT}/cadastrar`));
 
