00430e63ec12a9c1308b557d729371ae2f4f6770
Alisson <alissonsena784@gmail.com>
2026-05-18 10:36:52 -0300

    .

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const pages = require("./src/routes/pages.js");
-const PORT = 8080;
+const PORT = 3030;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(pages);
 
-app.listen(PORT, () => console.log(`SERVIDOR RODANDO COM SUCESSO. http://localhost:${PORT}/cadastrar`));
+app.listen(PORT, () => console.log(`SERVIDOR RODANDO COM SUCESSO. http://localhost:${PORT}/login`));
 
 
