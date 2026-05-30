87825b57f00fd5f587901d57d3302e2853bc75a2
Alisson <alissonsena784@gmail.com>
2026-05-30 15:25:12 -0300

    Desenvolvendo tela de inicio

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const path = require("path");
+import express, { json, static } from "express";
+import { join } from "path";
 const app = express();
-const pages = require("./src/routes/pages.js");
+import pages from "./src/routes/pages.js";
 const PORT = 3030;
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(json());
+app.use(static(join(__dirname, "public")));
 app.use(pages);
 
 app.listen(PORT, () => console.log(`SERVIDOR RODANDO COM SUCESSO. http://localhost:${PORT}/login`));
