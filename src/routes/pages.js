77f1ca748afdd1f236c4c9bbeebba8c0057eacfd
Alisson <alissonsena784@gmail.com>
2026-05-30 13:38:19 -0300

    branch nova

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -3,9 +3,7 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/cadastrar", (req, res) => res.sendFile(path.join(__dirname, "../../views/cadastro.html")));
-
 router.get("/login", (req, res) => res.sendFile(path.join(__dirname, "../../views/login.html")));
-
 router.get("/inicio", (req, res) => res.sendFile(path.join(__dirname, "../../views/inicio.html")));
 
 module.exports = router;
