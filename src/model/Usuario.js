98209998132883f9df47a5fa01757966fc7d509d
Matheus <matheusgubert88@gmail.com>
2026-06-02 22:01:05 -0300

    Novas features adicionadas, remocao da pasta routes, remocao do arquivo app

---
+++ src/model/Usuario.js
@@ -0,0 +1,16 @@
+export default class Usuario {
+
+    constructor(
+        id,
+        nomeCompleto,
+        email,
+        tipoPerfil
+    ) {
+
+        this.id = id;
+        this.nomeCompleto = nomeCompleto;
+        this.email = email;
+        this.tipoPerfil = tipoPerfil;
+    }
+
+}
